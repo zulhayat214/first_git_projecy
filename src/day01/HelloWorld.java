108ee3c83819ce5ec0475c68daf7566c0e6a3aa5
zulhayat214 <zulhayatyarmemet@gmail.com>
2024-12-30 09:50:47 -0600

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,6 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello World");
+         System.out.println("hello cydeo");
     }
 }
